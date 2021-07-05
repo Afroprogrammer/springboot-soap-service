caf378484523deb1a44aed0984c4c33e35e19036
Franklin <forsonfranklin@gmail.com>
2021-07-05 22:27:15 +0000

    service file with dummy data

---
+++ src/main/java/com/example/springbootsoapservice/service/UserService.java
@@ -0,0 +1,44 @@
+package com.example.springbootsoapservice.service;
+
+
+import com.example.springboot_soap_service.User;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UserService {
+    private static final Map<String, User> users = new HashMap<>();
+
+    @PostConstruct
+    public void intialize(){
+        User franklio = new User();
+        franklio.setUser("franklin");
+        franklio.setEmpId(112244);
+        franklio.setSalary(2.00);
+
+        User innocent = new User();
+        innocent.setUser("Innocent");
+        innocent.setEmpId(114);
+        innocent.setSalary(2000.00);
+
+
+        User Erastus = new User();
+        Erastus.setUser("Erastus");
+        Erastus.setEmpId(14);
+        Erastus.setSalary(25446.00);
+
+
+
+        users.put(franklio.getUser(), franklio);
+        users.put(innocent.getUser(), innocent);
+        users.put(Erastus.getUser(), Erastus);
+    }
+
+    public User getUser(String name)
+    {
+        return users.get(name);
+    }
+}
